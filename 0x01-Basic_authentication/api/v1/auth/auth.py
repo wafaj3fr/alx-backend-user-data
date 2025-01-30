02fb257b4dc05f2bdaaac88beaacfd811ea1ffbc
wafa <wafaagaafar@gmail.com>
2025-01-30 18:23:11 +0000

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,7 +21,11 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Retrieves the authorization header from the request """
-        return None
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Retrieves the current authenticated user (to be implemented later) """
