6ff33e81a96d3f0d4139ca1c6f1088ac8ec3ab30
wafa <wafaagaafar@gmail.com>
2025-01-30 17:20:13 +0000

    revised files

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,19 +26,11 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', strict_slashes=False)
-def unauthorized() -> None:
-    """GET /api/v1/unauthorized
-    Return:
-      - Unauthorized error.
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Raises:
+      - 401 error
     """
     abort(401)
 
-
-@app_views.route('/forbidden/', strict_slashes=False)
-def forbidden() -> None:
-    """GET /api/v1/forbidden
-    Return:
-      - Forbidden error.
-    """
-    abort(403)
