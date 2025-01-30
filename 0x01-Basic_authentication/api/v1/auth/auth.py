3bfb8b1cb33bfa499c0d93774e6d365633060b95
wafa <wafaagaafar@gmail.com>
2025-01-30 17:29:14 +0000

    auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Authentication module """
+
+from flask import request
+from typing import List, TypeVar
+
+class Auth:
+    """ Template for all authentication systems """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Determines if authentication is required for a given path """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Retrieves the authorization header from the request """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Retrieves the current authenticated user (to be implemented later) """
+        return None
+
