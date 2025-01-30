43a36813d764a17069d992242a4e19a03a9a64fb
wafa <wafaagaafar@gmail.com>
2025-01-30 18:49:11 +0000

    starter

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,46 +1,34 @@
 #!/usr/bin/env python3
-"""Authentication module for the API.
 """
-import os
-import re
-from typing import List, TypeVar
+Auth module for the API
+"""
 from flask import request
-
+from typing import List, TypeVar
 
 class Auth:
-    """Authentication class.
-    """
+    """ Template for all authentication systems """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if a path requires authentication.
-        """
-        if path is not None and excluded_paths is not None:
-            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
-                pattern = ''
-                if exclusion_path[-1] == '*':
-                    pattern = '{}.*'.format(exclusion_path[0:-1])
-                elif exclusion_path[-1] == '/':
-                    pattern = '{}/*'.format(exclusion_path[0:-1])
-                else:
-                    pattern = '{}/*'.format(exclusion_path)
-                if re.match(pattern, path):
-                    return False
+        """ Determines if authentication is required for a given path """
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+        path = path.rstrip('/') + '/'
+        for ex_path in excluded_paths:
+            if ex_path.rstrip('/') + '/' == path:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """Gets the authorization header field from the request.
-        """
-        if request is not None:
-            return request.headers.get('Authorization', None)
-        return None
+        """ Retrieves the authorization header from the request """
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Gets the current user from the request.
-        """
+        """ Placeholder for current user method """
         return None
 
-    def session_cookie(self, request=None) -> str:
-        """Gets the value of the cookie named SESSION_NAME.
-        """
-        if request is not None:
-            cookie_name = os.getenv('SESSION_NAME')
-            return request.cookies.get(cookie_name)
--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Docs """
+""" DocDocDocDocDocDoc
+"""
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
@@ -7,3 +8,4 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.users import *
 
+User.load_from_file()
